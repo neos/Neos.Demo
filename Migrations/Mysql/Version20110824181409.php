7e11b9c51802d3df7508f18a0cb9d57279377d64
Karsten Dambekalns <karsten@typo3.org>
2011-08-25 15:59:22 +0200

    [TASK] Add migration to create table for Registration entity
    The table is not used, as the entity is never persisted, but anyway. It
    is already using a FQCN-based table name.
    
    Change-Id: I1dc6385c75cf3dee9595e2bbc1ce570bdd538075
    Related: #29213

---
+++ Migrations/Mysql/Version20110824181409.php
@@ -0,0 +1,33 @@
+<?php
+namespace TYPO3\FLOW3\Persistence\Doctrine\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+	Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Migration for the (transient) Registration entity
+ */
+class Version20110824181409 extends AbstractMigration {
+
+	/**
+	 * @param Schema $schema
+	 * @return void
+	 */
+	public function up(Schema $schema) {
+		$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+
+		$this->addSql("CREATE TABLE typo3_phoenixdemotypo3org_domain_model_registration (flow3_persistence_identifier VARCHAR(40) NOT NULL, username VARCHAR(255) DEFAULT NULL, password VARCHAR(255) DEFAULT NULL, firstname VARCHAR(255) DEFAULT NULL, lastname VARCHAR(255) DEFAULT NULL, PRIMARY KEY(flow3_persistence_identifier)) ENGINE = InnoDB");
+	}
+
+	/**
+	 * @param Schema $schema
+	 * @return void
+	 */
+	public function down(Schema $schema) {
+		$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+
+		$this->addSql("DROP TABLE typo3_phoenixdemotypo3org_domain_model_registration");
+	}
+}
+
+?>
