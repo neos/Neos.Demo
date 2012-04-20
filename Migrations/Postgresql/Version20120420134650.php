45c0d1ee9e508bb71d261e0eb2802a9ae6d0f6e8
Karsten Dambekalns <karsten@typo3.org>
2012-04-20 13:53:30 +0200

    [TASK] Add DB migration for PostgreSQL
    This adds an initial migration for PostgreSQL to create the needed
    schema for the package.
    
    Change-Id: Ic07015fedb6d49d0ed657797191a4386f1e5fc22
    Related: #36301

---
+++ Migrations/Postgresql/Version20120420134650.php
@@ -0,0 +1,33 @@
+<?php
+namespace TYPO3\FLOW3\Persistence\Doctrine\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+	Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Create tables for PostgreSQL
+ */
+class Version20120420134650 extends AbstractMigration {
+
+	/**
+	 * @param Schema $schema
+	 * @return void
+	 */
+	public function up(Schema $schema) {
+		$this->abortIf($this->connection->getDatabasePlatform()->getName() != "postgresql");
+
+		$this->addSql("CREATE TABLE typo3_phoenixdemotypo3org_domain_model_registration (flow3_persistence_identifier VARCHAR(40) NOT NULL, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, PRIMARY KEY(flow3_persistence_identifier))");
+	}
+
+	/**
+	 * @param Schema $schema
+	 * @return void
+	 */
+	public function down(Schema $schema) {
+		$this->abortIf($this->connection->getDatabasePlatform()->getName() != "postgresql");
+
+		$this->addSql("DROP TABLE typo3_phoenixdemotypo3org_domain_model_registration");
+	}
+}
+
+?>
