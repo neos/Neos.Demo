7ffd14ef2050b8ef5071c5ffe0ec5026d2d2c3cc
Karsten Dambekalns <karsten@typo3.org>
2012-04-20 13:52:53 +0200

    [TASK] Adjust columns to NOT NULL default.
    Change-Id: Iab804a1619c393f45e72af1212b37853255df1c4
    Related: #35059

---
+++ Migrations/Mysql/Version20120420132456.php
@@ -0,0 +1,33 @@
+<?php
+namespace TYPO3\FLOW3\Persistence\Doctrine\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+	Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Adjust columns to NOT NULL
+ */
+class Version20120420132456 extends AbstractMigration {
+
+	/**
+	 * @param Schema $schema
+	 * @return void
+	 */
+	public function up(Schema $schema) {
+		$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+
+		$this->addSql("ALTER TABLE typo3_phoenixdemotypo3org_domain_model_registration CHANGE username username VARCHAR(255) NOT NULL, CHANGE password password VARCHAR(255) NOT NULL, CHANGE firstname firstname VARCHAR(255) NOT NULL, CHANGE lastname lastname VARCHAR(255) NOT NULL");
+	}
+
+	/**
+	 * @param Schema $schema
+	 * @return void
+	 */
+	public function down(Schema $schema) {
+		$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+
+		$this->addSql("ALTER TABLE typo3_phoenixdemotypo3org_domain_model_registration CHANGE username username VARCHAR(255) DEFAULT NULL, CHANGE password password VARCHAR(255) DEFAULT NULL, CHANGE firstname firstname VARCHAR(255) DEFAULT NULL, CHANGE lastname lastname VARCHAR(255) DEFAULT NULL");
+	}
+}
+
+?>
