69c55f6b0483c97ede7c8db704749d309b51c972
Sebastian Kurfürst <sebastian@typo3.org>
2013-10-07 14:48:08 +0200

    [!!!][FEATURE] New Demo Site
    Please also see the related changesets in the same
    topic branch for testing this.
    
    Change-Id: I9bafa9343857450a219db3f3d0828907dd7c78e0

--- Resources/Public/JavaScript/main.js
+++ Resources/Public/JavaScript/main.js
@@ -1,58 +1,16 @@
-(function($) {
-	// Calculate fixed tabs width
-	$.fn.calculateTabsWidth = function() {
+(function(scope, $) {
+	var document = scope.document,
+		location = scope.location;
 
-	$(this).each(function() {
-
-		var o = $(this);
-		var list = $(o).find('li');
-		var oWidth = o.outerWidth(true);
-
-		o.parent().width(oWidth); // fix for FF4 :)
-
-		var tabsNo = $(list).length;
-		tabsNo = (tabsNo == 0) ? 1 : tabsNo;
-
-		var tabsWidth = 0, addWidth = 0, mod = 0, counter = 0;
-
-		$(list).each(function() {
-			tabsWidth += $(this).outerWidth(true);
-		});
-
-		mod = (oWidth - tabsWidth) % tabsNo;
-		addWidth = (oWidth - mod - tabsWidth) / tabsNo;
-
-		$(list).each(function() {
-
-			newWidth = (counter < mod) ? $(this).width() + addWidth + 1 : $(this).width() + addWidth;
-
-			$(this).css({'width': newWidth});
-			$(this).find('a').css({'width': newWidth-1}); // for IE7 fix
-
-			counter++;
+	$(function() {
+		/**
+		 * Check if page is scrolled and switch 'scrolling' class for top navigation.
+		 * In our case this adds a nice shadow effect.
+		 */
+		var $topNavigationWrap = $('.top-navigation-wrap');
+		$(scope).on('scrollstart scrollend', function() {
+			$topNavigationWrap.toggleClass('scrolling', $(document).scrollTop() > 0);
 		});
 
 	});
-
-	}
-
-})(jQuery);
-
-$(document).ready(function(){
-	$('body').removeClass('js-off');
-
-	$(".tabs:not(.js-off):not(.search-result)").tabs("> .tab-panes > div", {tabs: 'li', current: 'act', initialIndex: 0});
-
-	// Home page Main Scroller/ Tabs
-	if ($('#top-slider').length > 0) {
-		$('#top-slider .slider-nav').tabs('#top-slider .slides > .slide', {tabs: 'li', current: 'active', effect: 'fade', fadeInSpeed: 1000, fadeOutSpeed: 1000, rotate: true}).slideshow({autoplay:true,interval:8000});
-	}
-
-	$('.lightbox').colorbox();
-	// END Home page Main Scroller/ Tabs
-
-	$('.vimeoBadge a').live('click', function() {
-		$(this).attr('target', '_blank');
-	});
-
-});
+})(window, jQuery);
