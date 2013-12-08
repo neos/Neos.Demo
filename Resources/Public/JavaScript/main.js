9344002ff679a72bf995d5554ee38c989ff9e362
Sebastian Helzle <sebastian@helzle.net>
2013-12-08 12:19:07 +0100

    [TASK] Improved responsive styling for demo site.
    Removed excessive white space in breadcrumb in mobile view.
    Body padding is computed in relation to the navbars height.
    Chapter navigation has less white space in mobile view.
    Main navigation links have full width and second level menu is aligned to center.
    Bit of refactoring in javascript.
    
    Change-Id: I4c68c40e9ac68ce23ca0428402e948ba5626c4c2

--- Resources/Public/JavaScript/main.js
+++ Resources/Public/JavaScript/main.js
@@ -1,15 +1,27 @@
 (function(scope, $) {
 	var document = scope.document,
-		location = scope.location;
+			$scope = $(scope),
+			$document = $(document),
+			$body = $('body');
 
 	$(function() {
+		var $topNavigationWrap = $('.top-navigation-wrap');
+
+		/**
+		 * Set the navbar fixed and update the body padding when the window resizes.
+		 * This is required to move the content below the navbar when scrolled to the top.
+		 */
+		$topNavigationWrap.addClass('navbar-fixed-top');
+		$scope.on('resize.topnav', function() {
+			$body.css('paddingTop', $topNavigationWrap.outerHeight());
+		}).resize();
+
 		/**
 		 * Check if page is scrolled and switch 'scrolling' class for top navigation.
 		 * In our case this adds a nice shadow effect.
 		 */
-		var $topNavigationWrap = $('.top-navigation-wrap');
-		$(scope).on('scrollstart scrollend', function() {
-			$topNavigationWrap.toggleClass('scrolling', $(document).scrollTop() > 0);
+		$scope.on('scrollstart scrollend', function() {
+			$topNavigationWrap.toggleClass('scrolling', $document.scrollTop() > 0);
 		});
 
 	});
