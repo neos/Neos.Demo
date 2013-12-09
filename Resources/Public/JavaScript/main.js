19f14ecbca6d850e94ebc412e1debf0036870abf
Sebastian Helzle <sebastian@helzle.net>
2013-12-09 01:43:28 +0100

    [TASK] Refactoring and js fixes for demo site.
    Replaced comments with hashes to double dashes.
    Topnav will behave correctly when loading other pages in backend.
    Fixed typos and removed some lost content elements.
    Added the contact form again to the plugins page and disabled the mail finisher.
    The youtube element is now responsive.
    Some other minor improvements.
    
    Depends on https://review.typo3.org/#/c/26081
    
    Change-Id: I106edf2c0e533f3d3d7ecda2e617a3891e299002

--- Resources/Public/JavaScript/main.js
+++ Resources/Public/JavaScript/main.js
@@ -2,19 +2,29 @@
 	var document = scope.document,
 			$scope = $(scope),
 			$document = $(document),
-			$body = $('body');
+			$body = $('body'),
+			$topNavigationWrap;
+
+	function updateBodyPadding() {
+		var paddingTop = $topNavigationWrap.length > 0 ? $topNavigationWrap.outerHeight() : 20;
+		$body.css('paddingTop', paddingTop);
+	}
+
+	function initTopNavigationWrap() {
+		$topNavigationWrap = $('.top-navigation-wrap').addClass('navbar-fixed-top');
+		updateBodyPadding();
+	}
 
 	$(function() {
 		var $topNavigationWrap = $('.top-navigation-wrap');
 
 		/**
-		 * Set the navbar fixed and update the body padding when the window resizes.
+		 * Set the navbar fixed on pageload and update the body padding when the window resizes or neos refreshes the backend.
 		 * This is required to move the content below the navbar when scrolled to the top.
 		 */
-		$topNavigationWrap.addClass('navbar-fixed-top');
-		$scope.on('resize.topnav', function() {
-			$body.css('paddingTop', $topNavigationWrap.outerHeight());
-		}).resize();
+		initTopNavigationWrap();
+		$scope.on('resize.topnav', updateBodyPadding);
+		document.addEventListener('Neos.PageLoaded', initTopNavigationWrap, false);
 
 		/**
 		 * Check if page is scrolled and switch 'scrolling' class for top navigation.
