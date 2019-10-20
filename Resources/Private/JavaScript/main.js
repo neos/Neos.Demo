bbf0a1d6f5f17a9095564219b18199d5f2bbe921
Jon Uhlmann <me@jonnitto.ch>
2019-10-20 20:47:45 +0200

    BUGFIX: Fix missing variable and remove unneded function

--- Resources/Private/JavaScript/main.js
+++ Resources/Private/JavaScript/main.js
@@ -1,10 +1,9 @@
 (function($) {
-    $('.top-navigation-wrap').addClass('navbar-fixed-top');
-
     /**
      * Check if page is scrolled and switch 'scrolling' class for top navigation.
      * In our case this adds a nice shadow effect.
      */
+    let $topNavigationWrap = $('.top-navigation-wrap');
     $(window).on('scrollstart scrollend', function() {
         $topNavigationWrap.toggleClass('scrolling', $(document).scrollTop() > 0);
     });
