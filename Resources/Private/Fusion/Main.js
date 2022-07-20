51cbe73e4bd31491d143b6ceff3c2826755407b6
Jon Uhlmann <account@uhlmann.pro>
2022-07-20 15:27:30 +0200

    TASK: Add header presentation

--- Resources/Private/Fusion/Main.js
+++ Resources/Private/Fusion/Main.js
@@ -3,6 +3,7 @@ import focus from '@alpinejs/focus';
 import intersect from '@alpinejs/intersect';
 import collapse from '@alpinejs/collapse';
 import persist from '@alpinejs/persist';
+import './Presentation/Section/Header';
 
 Alpine.plugin(focus);
 Alpine.plugin(intersect);
---
+++ Resources/Private/Fusion/Presentation/Section/Header/index.js
@@ -0,0 +1,54 @@
+import Alpine from 'alpinejs';
+
+Alpine.data('header', () => ({
+    mobileMenuOpen: false,
+    showShaow: false,
+    desktopView: true,
+    isCurrentPage(path) {
+        return window.location.pathname.startsWith(path);
+    },
+    onResize() {
+        this.mobileMenuOpen = false;
+
+        // Check if the mobile menu button is hidden
+        this.desktopView = !this.$refs.mobileMenuButton.offsetParent;
+    },
+    onScroll() {
+        this.showShaow = this.desktopView && window.pageYOffset;
+    },
+    init() {
+        this.onResize();
+        this.onScroll();
+    },
+    header: {
+        '@resize.window.passive.throttle'() {
+            this.onResize();
+        },
+        '@scroll.window.passive.throttle'() {
+            this.onScroll();
+        },
+        '@scroll.window.passive.debounce'() {
+            this.onScroll();
+        },
+        ':class'() {
+            return this.showShaow && 'shadow-lg';
+        },
+        '@keydown.window.escape'() {
+            this.mobileMenuOpen = false;
+        },
+        'x-trap.noscroll.inert'() {
+            return this.mobileMenuOpen;
+        },
+        '@click.outside'() {
+            this.mobileMenuOpen = false;
+        },
+    },
+}));
+
+Alpine.data('language', () => ({
+    open: false,
+    current: document.documentElement.lang?.replace('-', '_'),
+    getLink(language) {
+        return document.querySelector(`link[rel="alternate"][hreflang="${language.replace('_', '-')}"]`)?.href;
+    },
+}));
