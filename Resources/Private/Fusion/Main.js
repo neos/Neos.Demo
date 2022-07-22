db948070da73a947cd8f217400ff7f7c95fb5e56
Jon Uhlmann <account@uhlmann.pro>
2022-07-22 13:08:43 +0200

    TASK: Remove not used plugins

--- Resources/Private/Fusion/Main.js
+++ Resources/Private/Fusion/Main.js
@@ -1,15 +1,11 @@
 import Alpine from 'alpinejs';
 import focus from '@alpinejs/focus';
-import intersect from '@alpinejs/intersect';
 import collapse from '@alpinejs/collapse';
-import persist from '@alpinejs/persist';
 import './Presentation/Section/Header';
 import './Presentation/Section/Splide';
 
 Alpine.plugin(focus);
-Alpine.plugin(intersect);
 Alpine.plugin(collapse);
-Alpine.plugin(persist);
 
 // We use this to enable the AlpineJS debugger in the browser.
 if (document.documentElement.classList.contains('development')) {
